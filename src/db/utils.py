bd155ff3f506655b05240608f55da7c5ee615c78
dattlab <122553391+dattlab@users.noreply.github.com>
2023-07-22 01:40:26 +0800

    Refactor DBUtils

--- src/db/utils.py
+++ src/db/utils.py
@@ -1,84 +1,75 @@
-# Third party modules
 import pymongo
 import pymongo.database
 
-# User modules
-from .config import (
+from src.helpers import encrypt_passwd, passwd_match
+from src.config import (
     MONGO_URI,
     DB_NAME,
-    USER_COLLECTION_NAME
+    USER_COLLECTION_NAME,
+    POLL_COLLECTION_NAME
 )
 
 
-def get_database(db_name: str = DB_NAME) -> pymongo.database.Database:
-    """
-    Connects to a MongoClient with provided Mongo
-    URI connection string and returns a database
+class DBUtils:
+    def __init__(
+        self,
+        mongo_uri: str = MONGO_URI,
+        db_name: str = DB_NAME,
+        user_collection_name: str = USER_COLLECTION_NAME,
+        poll_collection_name: str = POLL_COLLECTION_NAME
+    ) -> None:
+        self.db = pymongo.MongoClient(mongo_uri)[db_name]
+        self.user_collection = self.db[user_collection_name]
+        self.poll_collection = self.db[poll_collection_name]
 
-    Parameters
-    ----------
-    db_name : str
+    def add_new_user(self, user_data: dict) -> None:
+        if not self.username_in_db(user_data["username"]):
+            user_data["passwd"] = encrypt_passwd(user_data["passwd"])
+            self.user_collection.insert_one(user_data)
 
-    Returns
-    -------
-    mongo.database.Database
+    def add_new_candidate(self, poll_name, candidate_name) -> None:
+        if self.poll_exists(poll_name):
+            new_candidate = {
+                "candidate_name": candidate_name,
+                "vote_count": 0
+            }
 
-    """
-    client = pymongo.MongoClient(MONGO_URI)
+            poll = self.poll_collection.find_one({"poll_name": poll_name})
 
-    return client[db_name]
+            new_candidates_list = poll["candidates"].append(new_candidate)
 
+            self.poll_collection.update_one({
+                "_id": poll["_id"]
+            }, {
+                "$set": {
+                    "candidates": new_candidates_list
+                }
+            }, upsert=False)
 
-def get_collection(collection_name: str, db_name=DB_NAME) -> pymongo.collection.Collection:
-    """
-    Get a collection from a given database name
+    def add_new_poll(self, poll_data: dict) -> None:
+        self.poll_collection.insert_one(poll_data)
 
-    Parameters
-    ----------
-    collection_name : str
-    db_name: str
+    def purge_collections(self) -> None:
+        self.user_collection.delete_many({})
+        self.poll_collection.delete_many({})
 
-    Returns
-    -------
-    mongo.collection.Collection
+    def username_in_db(self, username: str) -> bool:
+        return True if self.user_collection.find_one({"username": username}) \
+            else False
 
-    """
+    def poll_exists(self, poll_name: str) -> bool:
+        return True if self.poll_collection.find_one({"poll_name": poll_name}) \
+            else False
 
-    db_name = get_database(db_name)
-    collection = db_name[collection_name]
+    def valid_credential(self, input_user_data: dict) -> bool:
+        # Get the user entry from the database
+        # that matches with input user data
+        if self.username_in_db(input_user_data["username"]):
+            stored_user_data = self.user_collection.find_one({
+                "username": input_user_data["username"]
+            })
 
-    return collection
+            if passwd_match(input_user_data["passwd"], stored_user_data["passwd"]):
+                return True
 
-
-def insert_new_user(user_data: dict) -> None:
-    """
-    Insert new user to users collection
-
-    Parameters
-    ----------
-    user_data: dict
-        
-    """
-    user_collection = get_collection(USER_COLLECTION_NAME)
-    user_collection.insert_one(user_data)
-
-
-def is_username_taken(input_username: str) -> bool:
-    """
-    Checks if a user in data list
-    is already in the given collection
-
-    Parameters
-    ----------
-    input_username: str
-
-    Returns
-    -------
-    bool
-        
-    """
-
-    user_collection = get_collection(USER_COLLECTION_NAME)
-
-    return True if user_collection.find_one({'username': input_username}) \
-        else False
+        return False
