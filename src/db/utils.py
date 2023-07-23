877818d850284fa0614e9d835706567bd6439190
dattlab <122553391+dattlab@users.noreply.github.com>
2023-07-23 22:18:03 +0800

    Update utils
    - Fix add_new_user to check if generated voter_id is already in database
    - Fix appending new candidate in add_new_candidate
    - Add function to update candidate vote count
    - Add function to update user vote history
    - Add function to check if user already voted

--- src/db/utils.py
+++ src/db/utils.py
@@ -25,11 +25,19 @@ class DBUtils:
         self.position_collection = self.db[position_collection_name]
 
     def add_new_user(self, name: str, username: str, passwd: str) -> None:
+        global voter_id
+
+        voter_id_in_db = self.get_all_voters_id()
+
         if not self.username_in_db(username):
             passwd = encrypt_passwd(passwd)
 
             # Create random 5-digit voter ID
-            voter_id = random.randint(10000, 99999)
+            voter_id_already_in_db = True
+            while voter_id_already_in_db:
+                voter_id = random.randint(10000, 99999)
+                if voter_id not in voter_id_in_db:
+                    voter_id_already_in_db = False
 
             self.user_collection.insert_one({
                 "name": name,
@@ -40,7 +48,7 @@ class DBUtils:
                 "vote_history": []
             })
 
-    def add_new_candidate(self, position_name, candidate_name) -> None:
+    def add_new_candidate(self, position_name:str, candidate_name: str) -> None:
         if self.position_exists(position_name):
             new_candidate = {
                 "candidate_name": candidate_name,
@@ -49,7 +57,8 @@ class DBUtils:
 
             pos = self.position_collection.find_one({"position_name": position_name})
 
-            new_candidates_list = pos["candidates"].append(new_candidate)
+            new_candidates_list = pos["candidates"]
+            new_candidates_list.append(new_candidate)
 
             self.position_collection.update_one({
                 "_id": pos["_id"]
@@ -58,6 +67,41 @@ class DBUtils:
                     "candidates": new_candidates_list
                 }
             }, upsert=False)
+
+    def update_candidate_vote_count(self, position_name: str, candidate_name: str) -> None:
+        pos = self.position_collection.find_one({
+            "position_name": position_name,
+            "candidates.candidate_name": candidate_name
+        })
+
+        self.position_collection.update_one({
+            "_id": pos["_id"],
+            "candidates.candidate_name": candidate_name
+        }, {
+            "$inc": {
+                "candidates.$.vote_count": 1
+            }
+        })
+
+    def update_vote_history(self, username: str, position_name: str, candidate_name: str) -> None:
+        self.update_candidate_vote_count(position_name, candidate_name)
+
+        candidate = self.user_collection.find_one({"username": username})
+
+        vote_history = candidate["vote_history"]
+
+        vote_history.append({
+            "pos_name": position_name,
+            "chosen_candidate": candidate_name
+        })
+
+        self.user_collection.update_one({
+            "_id": candidate["_id"]
+        }, {
+            "$set": {
+                "vote_history": vote_history
+            }
+        }, upsert=False)
 
     def add_new_pos(self, position_data: dict) -> None:
         self.position_collection.insert_one(position_data)
@@ -80,6 +124,23 @@ class DBUtils:
     def purge_collections(self) -> None:
         self.user_collection.delete_many({})
         self.position_collection.delete_many({})
+
+    def user_already_voted(self, position_name: str, username: str) -> bool:
+        user_vote_history = self.user_collection.find_one({ "username": username })
+
+        # List of positions where the user already voted in
+        pos_voted = [v["pos_name"] for v in user_vote_history["vote_history"]]
+
+        if position_name in pos_voted:
+            return True
+
+        return False
+
+    def user_is_admin(self, username: str) -> bool:
+        user = self.user_collection.find_one({ "username": username })
+        if user["is_admin"]:
+            return True
+        return False
 
     def username_in_db(self, username: str) -> bool:
         return True if self.user_collection.find_one({"username": username}) \
