a9a97565d9524e26fd3ef24b3a68f4573cf45aec
dattlab <122553391+dattlab@users.noreply.github.com>
2023-07-23 20:26:47 +0800

    Update utils
    - Change name for position collection
    - Change add_new_user input implementation
    - Add function for collecting voter ids
    - Add function for importing predefined data from JSON file
    - Change valid_credential input implementation

--- src/db/utils.py
+++ src/db/utils.py
@@ -1,3 +1,5 @@
+import json
+import random
 import pymongo
 import pymongo.database
 
@@ -6,7 +8,7 @@ from src.config import (
     MONGO_URI,
     DB_NAME,
     USER_COLLECTION_NAME,
-    POLL_COLLECTION_NAME
+    POS_COLLECTION_NAME
 )
 
 
@@ -16,60 +18,86 @@ class DBUtils:
         mongo_uri: str = MONGO_URI,
         db_name: str = DB_NAME,
         user_collection_name: str = USER_COLLECTION_NAME,
-        poll_collection_name: str = POLL_COLLECTION_NAME
+        position_collection_name: str = POS_COLLECTION_NAME
     ) -> None:
         self.db = pymongo.MongoClient(mongo_uri)[db_name]
         self.user_collection = self.db[user_collection_name]
-        self.poll_collection = self.db[poll_collection_name]
+        self.position_collection = self.db[position_collection_name]
 
-    def add_new_user(self, user_data: dict) -> None:
-        if not self.username_in_db(user_data["username"]):
-            user_data["passwd"] = encrypt_passwd(user_data["passwd"])
-            self.user_collection.insert_one(user_data)
+    def add_new_user(self, name: str, username: str, passwd: str) -> None:
+        if not self.username_in_db(username):
+            passwd = encrypt_passwd(passwd)
 
-    def add_new_candidate(self, poll_name, candidate_name) -> None:
-        if self.poll_exists(poll_name):
+            # Create random 5-digit voter ID
+            voter_id = random.randint(10000, 99999)
+
+            self.user_collection.insert_one({
+                "name": name,
+                "username": username,
+                "passwd": passwd,
+                "voter_id": voter_id,
+                "is_admin": False,
+                "vote_history": []
+            })
+
+    def add_new_candidate(self, position_name, candidate_name) -> None:
+        if self.position_exists(position_name):
             new_candidate = {
                 "candidate_name": candidate_name,
                 "vote_count": 0
             }
 
-            poll = self.poll_collection.find_one({"poll_name": poll_name})
+            pos = self.position_collection.find_one({"position_name": position_name})
 
-            new_candidates_list = poll["candidates"].append(new_candidate)
+            new_candidates_list = pos["candidates"].append(new_candidate)
 
-            self.poll_collection.update_one({
-                "_id": poll["_id"]
+            self.position_collection.update_one({
+                "_id": pos["_id"]
             }, {
                 "$set": {
                     "candidates": new_candidates_list
                 }
             }, upsert=False)
 
-    def add_new_poll(self, poll_data: dict) -> None:
-        self.poll_collection.insert_one(poll_data)
+    def add_new_pos(self, position_data: dict) -> None:
+        self.position_collection.insert_one(position_data)
+
+    def get_all_voters_id(self) -> list[int]:
+        users = list(self.user_collection.find({}))
+        voter_id_list = []
+        for user in users:
+            if user["is_admin"] == False:
+                voter_id_list.append(user["voter_id"])
+
+        return voter_id_list
+
+    def import_collection_from_json(self, json_file_path: str, collection_name: str) -> None:
+        with open(json_file_path, "r") as f:
+            documents = json.loads(f.read())
+
+        self.db[collection_name].insert_many(documents)
 
     def purge_collections(self) -> None:
         self.user_collection.delete_many({})
-        self.poll_collection.delete_many({})
+        self.position_collection.delete_many({})
 
     def username_in_db(self, username: str) -> bool:
         return True if self.user_collection.find_one({"username": username}) \
             else False
 
-    def poll_exists(self, poll_name: str) -> bool:
-        return True if self.poll_collection.find_one({"poll_name": poll_name}) \
+    def position_exists(self, position_name: str) -> bool:
+        return True if self.position_collection.find_one({"position_name": position_name}) \
             else False
 
-    def valid_credential(self, input_user_data: dict) -> bool:
+    def valid_credential(self, username: str, passwd: str) -> bool:
         # Get the user entry from the database
         # that matches with input user data
-        if self.username_in_db(input_user_data["username"]):
+        if self.username_in_db(username):
             stored_user_data = self.user_collection.find_one({
-                "username": input_user_data["username"]
+                "username": username
             })
 
-            if passwd_match(input_user_data["passwd"], stored_user_data["passwd"]):
+            if passwd_match(passwd, stored_user_data["passwd"]):
                 return True
 
         return False
