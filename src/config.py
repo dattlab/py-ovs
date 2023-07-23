8404056e96dfe15587a8f6d187c5f81a8b9bbf85
dattlab <122553391+dattlab@users.noreply.github.com>
2023-07-23 20:24:19 +0800

    Update position schema and collection

--- src/config.py
+++ src/config.py
@@ -4,5 +4,5 @@ from dotenv import dotenv_values
 DB_NAME = "ovs"
 USER_COLLECTION_NAME = "users"
 CANDIDATE_COLLECTION_NAME = "candidates"
-POLL_COLLECTION_NAME = "polls"
+POS_COLLECTION_NAME = "positions"
 MONGO_URI = dotenv_values("src/.env")["MONGO_URI"]
